40a0284727df1e5bb5deb0163a360760a18b412e
Nolwenn-Borgne <nolwenn.borgne@gmail.com>
2021-05-05 05:53:01 +0200

    nelmio alice installed

--- facecook/src/DataFixtures/AppFixtures.php
+++ facecook/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        // // create 5 categories
+        // for ($i = 0; $i < 5; $i++) {
+        //     $category = new Category();
+        //     $category->setName('catégorie' . $i);
+        //     $manager->persist($category);
+        // }
+
+        // // create 10 ingredients
+        // for ($i = 0;)
+
 
         $manager->flush();
     }
