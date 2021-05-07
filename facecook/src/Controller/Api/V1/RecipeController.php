798510574e90aea6834f8405e7dc84024cb94d9e
Nolwenn-Borgne <nolwenn.borgne@gmail.com>
2021-05-07 14:47:03 +0200

    fix2 cors

--- facecook/src/Controller/Api/V1/RecipeController.php
+++ facecook/src/Controller/Api/V1/RecipeController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\CorsBundle\NelmioCorsBundle;
 
 /**
  * @Route("/api/v1/recipes", name="api_v1_recipes_")
--- facecook/src/Controller/Api/V1/UserController.php
+++ facecook/src/Controller/Api/V1/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Nelmio\CorsBundle\NelmioCorsBundle;
 
 /**
  * @Route("/api/v1/users", name="api_v1_users_")
