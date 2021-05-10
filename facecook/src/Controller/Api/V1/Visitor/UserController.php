c8bf4cc939d302bb0d9c0983261a3301b58d2bd9
Nolwenn-Borgne <nolwenn.borgne@gmail.com>
2021-05-10 16:34:16 +0200

    creation of the public UserController in order to manage access for inscription

---
+++ facecook/src/Controller/Api/V1/Visitor/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Api\V1\Visitor;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * @Route("/api/v1/public/users", name="api_v1_public_users_")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("", name="add", methods={"POST"})
+     */
+    public function add(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user, ['csrf_protection' => false]);
+
+        $sentData = json_decode($request->getContent(), true);
+        $form->submit($sentData);
+
+        if ($form->isValid()) {
+
+            // Before submitting the new user, the password needs to be hashed. 
+            $password = $form->get('password')->getData();
+            $user->setPassword($passwordEncoder->encodePassword($user, $password));
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->json($user, 201, []);
+            
+        }
+
+        return $this->json($form->getErrors(true, false)->__toString(), 400);
+    }
+}
